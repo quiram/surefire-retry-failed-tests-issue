6d9e3d9c5b4d490ff34b9b6e0052fa3e2e7af7d7
Abraham Marin-Perez <abraham.marin.perez@gmail.com>
2019-10-21 15:12:06 +0100

    SUREFIRE-1701 change tests so they pass once the functionality is fixed

--- src/test/java/com/github/quiram/SurefireIssueNoRetryTest.java
+++ src/test/java/com/github/quiram/SurefireIssueNoRetryTest.java
@@ -3,14 +3,20 @@ package com.github.quiram;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class SurefireIssueNoRetryTest {
+
+    private static int count = 0;
 
     @BeforeEach
     void setUp() {
-        fail("Forcing retry");
+        count++;
+        assertEquals(3, count);
+        count = 0;
     }
 
     @Test
